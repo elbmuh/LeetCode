69feb6342326ef247797a4b04e3efdc7f0dbf622
MinSeo-Hwang <117055111+elbmuh@users.noreply.github.com>
2023-08-14 18:17:59 +0900

    Update 383.py

--- Beginner/383.py
+++ Beginner/383.py
@@ -50,3 +50,32 @@ class Solution:
 
 
 # ======= case 04 ======= (출처 : 호놀률루 / 매일을 꿈틀대는 법)
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        ## ransomNote : 여기에서 글자들을 추출한다.
+        ## magazine : 추출한 글자들을 자신이 원하는 단어 형태로 만든다.
+        global num
+
+        ransomNote_list = list(ransomNote)
+        magazine_list = list(magazine)
+
+        if len(ransomNote_list) > len(magazine_list) :
+            return False
+        else : 
+            for i in range(0, len(ransomNote_list)) : 
+                for j in range(0, len(magazine_list)) : 
+                    num = 1
+
+                    if ransomNote_list[i] == magazine_list[j] : 
+                        num -= 1
+                        magazine_list.pop(j)
+                        break
+
+                if num == 1 :
+                    break
+
+        if num == 1 :
+            return False
+        
+        else : 
+            return True
